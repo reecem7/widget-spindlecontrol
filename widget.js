55baab2af1f50c0d360b1a1a4c5465aa0f1e91c2
Reece <reece.maganzini@gmail.com>
2017-12-11 05:07:30 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -347,5 +347,19 @@ cpdefine("inline:com-chilipeppr-widget-spindlecontrol", ["chilipeppr_ready", /* 
 
         },
 
+            
+
     }
+    
+    
 });
+/*
+            var slider = document.getElementById("myRange");
+            var output = document.getElementById("demo");
+            output.innerHTML = slider.value; // Display the default slider value
+            
+            // Update the current slider value (each time you drag the slider handle)
+            slider.oninput = function() {
+                output.innerHTML = this.value;
+            }
+            */
